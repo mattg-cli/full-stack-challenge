1e3f33453d1578f13161cac9a37ff4ded05ad00e
Matt Gilbert <mattgilbert042@gmail.com>
2020-12-31 10:45:13 -0600

    php mini project completed with CRUD operations and totalling price with tax

--- project_02.php
+++ project_02.php
@@ -1,21 +1,57 @@
 <?php
 
+// PHP Version: 8.0 (latest and greatest!)
+
+// Instructions:
+// 1. handle adding items, removing items and clearing all items from the cart, 
+        // then return the updated cart.
+// 2. calculate and return the total of all the items in the cart with tax applied
+
 class Cart
 {
 
-    /**
-     * Example of cart_items array structure
-     * $cart_items = ['product_id' => ['quantity' => quantity, 'unit_price' => unit_price]]
-     */
-
-    public $cart_items = [];
+    public $cart_items = array();
     public $tax_percent = 10;
-    private $price_list = [
-        // 'product_id' => price,
+    public $price_list = [
         '1001' => 12.99,
         '1002' => 9.27,
         '1003' => 10.00,
         '1004' => 4.19,
         '1005' => 6.75,
-    ];  
+    ];
+
+    public function getCartItems() {
+        return $this->cart_items;
+    }
+
+    public function addItems($id, $unit_price, $quantity) {
+        $this->cart_items[$id] = [
+            'unit_price' => $unit_price,
+            'quantity' => $quantity
+        ];
+        return $this->getCartItems();
+    }
+
+    public function deleteItem($id) {
+        unset($this->cart_items[$id]);
+        return $this->getCartItems();
+    }
+    
+    public function clearCartItems() {
+        unset($this->cart_items);
+        $this->cart_items = array();
+    }
+
+    public function totalCartPrice() {
+        $cart_total = 0;
+        $cart_tax = 0;
+
+        foreach ($this->cart_items as $item) {
+            $cart_total += $item['unit_price'] * $item['quanitity'];
+        }
+        $cart_tax = ($this->tax_percent / 100) * $cart_total;
+        
+        return round(($cart_tax + $cart_total), 2);
+    }
+
 }
